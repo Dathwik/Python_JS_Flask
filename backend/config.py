cadebea834e5b64d126fc6815d384d95bec1bc1b
DathwikK <satya.dathwik@gmail.com>
2026-03-05 12:16:29 -0500

    added DATABASE URL

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -5,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app,origins=["https://python-js-flask.onrender.com"])
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
