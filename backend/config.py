4dc518f1f370b6dd12d95f8ae02bc1ef10fcc04e
Dathwik Kollikonda <133098050+Dathwik@users.noreply.github.com>
2026-03-05 12:36:10 -0500

    Enhance CORS and refactor database URI assignment
    Updated CORS settings to support credentials and refactored database URI assignment.

--- backend/config.py
+++ backend/config.py
@@ -4,9 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app,origins=["https://python-js-flask.onrender.com"])
+CORS(app,origins=["https://python-js-flask.onrender.com"], supports_credentials = True)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
